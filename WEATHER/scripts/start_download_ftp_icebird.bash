e547b7699bac78fcac5683dce0661d84f1aae041
vludwig <valentin.ludwig@awi.de>
2022-07-28 17:55:48 +0200

    localpath now set depending on user

--- WEATHER/scripts/start_download_ftp_icebird.bash
+++ WEATHER/scripts/start_download_ftp_icebird.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 CONDAPATH="$HOME/opt/anaconda3"
-SCRIPTPATH="$HOME/ICEBIRD/WEATHER/scripts"
-LOGFILEPATH="$HOME/ICEBIRD/WEATHER/logfiles"
+if [ "$(whoami)" == "vludwig" ]; then
+	SCRIPTPATH="$HOME/04_EVENTS/03_ICEBIRD/03_REPO/WEATHER/scripts"
+	LOGFILEPATH="$HOME/04_EVENTS/03_ICEBIRD/03_REPO/WEATHER/logfiles"
+elif [ "$(whoami)" == "icebird" ]; then
+	SCRIPTPATH="$HOME/ICEBIRD/REPO/WEATHER/scripts"
+	LOGFILEPATH="$HOME/ICEBIRD/REPO/WEATHER/logfiles"
+fi
 source $CONDAPATH/bin/activate $CONDAPATH/envs/icebird
 timestamp=$(/bin/date +%Y%m%d_%H%M%S)
 python -W ignore $SCRIPTPATH/download_ftp_icebird.py>>$LOGFILEPATH/logfile_$timestamp.txt
