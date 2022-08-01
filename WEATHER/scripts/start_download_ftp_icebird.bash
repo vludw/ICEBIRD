1957bd0d39a3c714932f6c4a93c29a1672f63e1e
icebird <valentin.ludwig@awi.de>
2022-08-01 10:45:15 +0200

    Added line for doing daily backup, data of each day now additionally saved in a separate folder

--- WEATHER/scripts/start_download_ftp_icebird.bash
+++ WEATHER/scripts/start_download_ftp_icebird.bash
@@ -1,7 +1,21 @@
 #!/bin/bash
-CONDAPATH="$HOME/opt/anaconda3"
-SCRIPTPATH="$HOME/ICEBIRD/WEATHER/scripts"
-LOGFILEPATH="$HOME/ICEBIRD/WEATHER/logfiles"
-source $CONDAPATH/bin/activate $CONDAPATH/envs/icebird
-timestamp=$(/bin/date +%Y%m%d_%H%M%S)
-python -W ignore $SCRIPTPATH/download_ftp_icebird.py>>$LOGFILEPATH/logfile_$timestamp.txt
+## SET FILEPATHS AND TIMESTAMPS
+TIMESTAMP_LOGFILE=$(/bin/date +%Y%m%d_%H%M%S) # logfiles of download script will be saved here
+TIMESTAMP_BACKUP=$(/bin/date +%Y%m%d) # this will be used for naming the backup directory
+CONDAPATH="$HOME/opt/anaconda3" # conda environment for running the download script resides here
+SCRIPTPATH="$HOME/ICEBIRD/WEATHER/scripts" # path to this script
+LOGFILEPATH="$HOME/ICEBIRD/WEATHER/logfiles" # path for logfiles of download
+DATAPATH="$HOME/ICEBIRD/WEATHER/data" # path to which data will be downloaded
+BACKUPPATH="$HOME/ICEBIRD/WEATHER/daily_data/data_$TIMESTAMP_BACKUP" # path where backup will be saved
+
+## RUN DOWNLOAD
+source $CONDAPATH/bin/activate $CONDAPATH/envs/icebird # activate the conda environment
+python -W ignore $SCRIPTPATH/download_ftp_icebird.py>>$LOGFILEPATH/logfile_$TIMESTAMP_LOGFILE.txt # run download script
+
+## RUN BACKUP
+if [ ! -d $BACKUPPATH ] # check if backup directory exists, if not, create it
+then
+	mkdir -p $BACKUPPATH
+fi
+cp -pruv $DATAPATH/* $BACKUPPATH/>>$LOGFILEPATH/logfile_$TIMESTAMP_LOGFILE.txt # do the backup, append output to download logfile
+
