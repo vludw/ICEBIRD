f2422cb672374465735d47184344fe49b6a7682c
icebird <valentin.ludwig@awi.de>
2022-08-02 17:17:35 +0200

    Updated remote data folder on main after downloading only selected data

--- WEATHER/scripts/download_ftp_icebird.py
+++ WEATHER/scripts/download_ftp_icebird.py
@@ -17,6 +17,7 @@
 ##      - In function get_pathdict():
 ##          - remotepath: Filepath on remote server. For DWD, all the data are in subdirectories of one folder called "data", therefore "data" is set as remotepath.
 ##          - localpath: Filepathon your local machine to which the data shall be saved
+##      - In list skip_dir
 ## Workflow
 #   - Connect to SFTP server using Python's paramiko module  
 #   - Compare local and remote files
@@ -34,124 +35,130 @@ import os,sys # needed for filesize comparison
 from stat import S_ISDIR, S_ISREG # needed in sftp_get_recursive function
 
 ## Function which downloads the data recursively
-def sftp_get_recursive(path, dest, sftp, verbose = False, print_summary = True):
-    """
-        Function to recursively download all data in path to dest. Directory structure is preserved.
-        Input: 
-            - path: path on remote server (without sftp::// prefix)
-            - dest: local path to which data shall be saved
-            - sftp: SFTP object as returned by paramiko.SFTPClient.from_transport function
-            - verbose (optional): tell me whether or not a file was downloaded
-            - print_summary (optional): tell me how many files were downloaded/skipped in each directory
-            
-        - Output:
-            - no variables returned
-    """
-    item_list = sftp.listdir_attr(path) # all files and directories on remote path. Note that this does not differentiate between files and directories.
-    dest = str(dest) # make sure that local path is given as string
-    if not os.path.isdir(dest): # create local path if it does not exist yet
-        os.makedirs(dest, exist_ok=True)
-    downloaded,skipped = [],[] # lists fopr downloaded/skipped filenames
-    for item in item_list: # loop over files on remote server
-        mode = item.st_mode # needed to determine whether the remote entry is a file or a directory
-        filesize_remote = item.st_size # filesize of remote file
-        atime_remote = item.st_atime # access time of remote file
-        mtime_remote = item.st_mtime # modification time of remote file
-        file_exists = os.path.exists(os.path.join(dest,item.filename)) # check whether file exists locally
-        if file_exists:
-            filesize_local = os.path.getsize(os.path.join(dest,item.filename)) # get filesize of local file if it exists
-        else:
-            filesize_local = -999 # set local filesize to arbitrary fill value if it does not exist
-        if S_ISDIR(mode): # check if remote file is a directory
-            sftp_get_recursive(path + "/" + item.filename, dest + "/" + item.filename, sftp, verbose = verbose, print_summary = print_summary) # if remote file is a directory, repeat the function
-            if verbose == True:
-                print("Setting modification time of local file to modification time of remote file")
-            os.utime(os.path.join(dest,item.filename),(atime_remote,mtime_remote)) # change local modification and access time such that it is consistent with the remote one
-        else:
-            if not file_exists: # if the file does not exist at all, download it
-                if verbose == True:
-                    print(f"{os.path.join(dest,item.filename)} does not exist, I download it!")
-                sftp.get(path + "/" + item.filename, dest + "/" + item.filename) # download remote file
-                if verbose == True:
-                    print("Setting modification time of local file to modification time of remote file")
-                os.utime(os.path.join(dest,item.filename),(atime_remote,mtime_remote)) # change local modification and access time such that it is consistent with the remote one
-                downloaded.append(os.path.join(dest,item.filename)) # add filename to list of downloaded filenames
-            elif file_exists & (filesize_remote != filesize_local): # if the file exists locally, but the filesize differs from that of the remote file, download it
-                if verbose == True:
-                    print(f"{os.path.join(dest,item.filename)} exists, but size of local and remote file differ. I download it!")
-                sftp.get(f"{os.path.join(path,item.filename)}", f"{os.path.join(dest,item.filename)}") # download remote file
-                downloaded.append(os.path.join(dest,item.filename)) # add filename to list of downloaded filenames
-                if verbose == True:
-                    print("Setting modification time of local file to modification time of remote file")
-                os.utime(os.path.join(dest,item.filename),(atime_remote,mtime_remote))# change local modification and access time such that it is consistent with the remote one
-            else: # do nothing if file exists and filesizes are identical
-                if verbose == True:
-                    print(f"{os.path.join(dest,item.filename)} exists and size of local and remote file are identical, I skip it!")
-                skipped.append(os.path.join(dest,item.filename)) # add filename to listb of skipped filenames
-                pass
-    if print_summary: # print a summary (how many files were downloaded/skipped for each directory)
-        try: # if variable "item" exists, print the summary
-            print(f"{os.path.dirname(os.path.join(path,item.filename))}: {len(downloaded)} file(s) downloaded, {len(skipped)} file(s) skipped")
-        except UnboundLocalError: # if it does not exist, pass
-            pass
+def sftp_get_recursive(path, dest, download_names, sftp, verbose = False, print_summary = True):
+	"""
+	Function to recursively download all data in path to dest. Directory structure is preserved.
+	Input: 
+		- path: path on remote server (without sftp::// prefix)
+		- dest: local path to which data shall be saved
+		- sftp: SFTP object as returned by paramiko.SFTPClient.from_transport function
+		- verbose (optional): tell me whether or not a file was downloaded
+		- print_summary (optional): tell me how many files were downloaded/skipped in each directory
+	Output:
+		- no variables returned
+	"""
+	item_list = sftp.listdir_attr(path) # all files and directories on remote path. Note that this does not differentiate between files and directories.
+	dest = str(dest) # make sure that local path is given as string
+	if not os.path.isdir(dest): # create local path if it does not exist yet
+		os.makedirs(dest, exist_ok=True)
+	downloaded,skipped = [],[] # lists fopr downloaded/skipped filenames
+	for item in item_list: # loop over files on remote server
+		if not item.filename in download_names:
+			print(f"{item.filename} is not in download_names, will not be downloaded!")
+			continue
+		mode = item.st_mode # needed to determine whether the remote entry is a file or a directory
+		filesize_remote = item.st_size # filesize of remote file
+		atime_remote = item.st_atime # access time of remote file
+		mtime_remote = item.st_mtime # modification time of remote file
+		file_exists = os.path.exists(os.path.join(dest,item.filename)) # check whether file exists locally
+		if file_exists:
+			filesize_local = os.path.getsize(os.path.join(dest,item.filename)) # get filesize of local file if it exists
+		else:
+			filesize_local = -999 # set local filesize to arbitrary fill value if it does not exist
+		if S_ISDIR(mode): # check if remote file is a directory
+			sftp_get_recursive(path + "/" + item.filename, dest + "/" + item.filename, download_names,sftp, verbose = verbose, print_summary = print_summary) # if remote file is a directory, repeat the function
+			if verbose == True:
+				print("Setting modification time of local file to modification time of remote file")
+			os.utime(os.path.join(dest,item.filename),(atime_remote,mtime_remote)) # change local modification and access time such that it is consistent with the remote one
+		else:
+			if not file_exists: # if the file does not exist at all, download it
+				if verbose == True:
+					print(f"{os.path.join(dest,item.filename)} does not exist, I download it!")
+				sftp.get(path + "/" + item.filename, dest + "/" + item.filename) # download remote file
+				if verbose == True:
+					print("Setting modification time of local file to modification time of remote file")
+				os.utime(os.path.join(dest,item.filename),(atime_remote,mtime_remote)) # change local modification and access time such that it is consistent with the remote one
+				downloaded.append(os.path.join(dest,item.filename)) # add filename to list of downloaded filenames
+			elif file_exists & (filesize_remote != filesize_local): # if the file exists locally, but the filesize differs from that of the remote file, download it
+				if verbose == True:
+					print(f"{os.path.join(dest,item.filename)} exists, but size of local and remote file differ. I download it!")
+				sftp.get(f"{os.path.join(path,item.filename)}", f"{os.path.join(dest,item.filename)}") # download remote file
+				downloaded.append(os.path.join(dest,item.filename)) # add filename to list of downloaded filenames
+				if verbose == True:
+					print("Setting modification time of local file to modification time of remote file")
+				os.utime(os.path.join(dest,item.filename),(atime_remote,mtime_remote))# change local modification and access time such that it is consistent with the remote one
+			else: # do nothing if file exists and filesizes are identical
+				if verbose == True:
+					print(f"{os.path.join(dest,item.filename)} exists and size of local and remote file are identical, I skip it!")
+				skipped.append(os.path.join(dest,item.filename)) # add filename to listb of skipped filenames
+				pass
+	if print_summary: # print a summary (how many files were downloaded/skipped for each directory)
+		try: # if variable "item" exists, print the summary
+			print(f"{os.path.dirname(os.path.join(path,item.filename))}: {len(downloaded)} file(s) downloaded, {len(skipped)} file(s) skipped")
+		except UnboundLocalError: # if it does not exist, pass
+			pass
                 
 ## Access data for SFTP server
 def get_accessdict(): # get credentials and address for SFTP login
-    """
-    Get address and credentials of the SFTP server.
-    Input:
-        - no input required
-    Output:
-        - dictionary with host, port, password and username
-    """
-    host = "data.dwd.de" # host name
-    port = 2424 # port number  
-    password = "C7UFYvD7Rb.*4ub;gq;Y" # password
-    username = "awiarkti" # username
-    accessdict = {"host":host,"port":port,"password":password,"username":username} # dictionary with credentials
-    return accessdict
+	"""
+	Get address and credentials of the SFTP server.
+	Input:
+		- no input required
+	Output:
+		- dictionary with host, port, password and username
+	"""
+	host = os.getenv("HOST") # host name
+	port = int(os.getenv("PORT")) # host name
+	username = os.getenv("USERNAME") # host name
+	password = os.getenv("PASSWORD") # host name
+	accessdict = {"host":host,"port":port,"password":password,"username":username} # dictionary with credentials
+	return accessdict
 
 ## Filepaths on local machine and remote SFTP server
 def get_pathdict(): # get remote and local filepaths
-    """
-    Get filepaths.
-    Input:
-        - no input required
-    Output:
-        - dictionary with local and remote filepath
-    """
-    remotepath = "data" # set path on remote SFTP SERVER
-    localpath = os.path.join(os.getenv("HOME"),"ICEBIRD/WEATHER/data") # set path on local machine
-    pathdict = {"remotepath":remotepath,"localpath":localpath} # dictionary with paths
-    return pathdict
+	"""
+	Get filepaths.
+	Input:
+		- no input required
+	Output:
+		- dictionary with local and remote filepath
+	"""
+	remotepath = os.getenv("REMOTEPATH") # set path on remote SFTP SERVER
+	localpath = os.getenv("LOCALPATH") # set path on local machine
+	pathdict = {"remotepath":remotepath,"localpath":localpath} # dictionary with paths
+	return pathdict
 
 ## Connection to SFTP
 def open_sftp(accessdict):
-    """
-    Open connection to SFTP server and login.
-    Input:
-        - accessdict: dictionary with address and credentials, returned by function get_accessdict()
-    Output:
-        - SFTP and transport object which will be used for downloading
-    """
-    transport = paramiko.Transport((accessdict["host"], accessdict["port"])) # open connection    
-    transport.connect(username = accessdict["username"], password = accessdict["password"]) # connect to server    
-    sftp = paramiko.SFTPClient.from_transport(transport) # open SFTP connection
-    return sftp,transport
+	"""
+	Open connection to SFTP server and login.
+	Input:
+		- accessdict: dictionary with address and credentials, returned by function get_accessdict()
+	Output:
+		- SFTP and transport object which will be used for downloading
+	"""
+	transport = paramiko.Transport((accessdict["host"], accessdict["port"])) # open connection    
+	transport.connect(username = accessdict["username"], password = accessdict["password"]) # connect to server    
+	sftp = paramiko.SFTPClient.from_transport(transport) # open SFTP connection
+	return sftp,transport
 
 ## Close connection to SFTP server
 def close_sftp(sftp,transport):
-    """
-    Close connection to SFTP server.
-    Input:
-        - SFTP and transport object as returned by function open_sftp
-    Output:
-        - no output
-    """
-    sftp.close() # log out of SFTP server
-    transport.close() # close connection
+	"""
+	Close connection to SFTP server.
+	Input:
+		- SFTP and transport object as returned by function open_sftp
+	Output:
+		- no output
+	"""
+	sftp.close() # log out of SFTP server
+	transport.close() # close connection
 
 
+# Skip directories
+f_downloadnames = open("filelist.txt", mode = "r")
+f_lines_tmp = f_downloadnames.readlines()
+download_names = [f_line_tmp[:-1] for f_line_tmp in f_lines_tmp]
 # Get access data
 accessdict,pathdict = get_accessdict(), get_pathdict() # dictionaries are needed for the SFTP connection
 
@@ -159,7 +166,7 @@ accessdict,pathdict = get_accessdict(), get_pathdict() # dictionaries are needed
 sftp,transport = open_sftp(accessdict) # get SFTP object
 
 # Download data
-sftp_get_recursive(pathdict["remotepath"], pathdict["localpath"], sftp,verbose = False, print_summary = True) # download data
+sftp_get_recursive(pathdict["remotepath"], pathdict["localpath"],download_names,sftp,verbose = False, print_summary = True) # download data
 
 # Close connection
 close_sftp(sftp,transport) # close connection
--- WEATHER/scripts/start_download_ftp_icebird.bash
+++ WEATHER/scripts/start_download_ftp_icebird.bash
@@ -3,15 +3,9 @@
 ## INSTRUCTIONS
 ## You need a conda environment in which the Python module paramiko is installed. This should reside in the path specified by $CONDAPATH/envs. If it is not called icebird, change the name accordingly. If the scripts are at different locations than what is specified in this script, make sure to adapt the filepaths. Then, the script can be run by /bin/bash path_to_script/scriptname.bash, possibly after making it executable by using chmod +x scriptname.bash. Output is written to a logfile in $LOGFILEPATH.
 
-## SET FILEPATHS AND TIMESTAMPS
-TIMESTAMP_LOGFILE=$(/bin/date +%Y%m%d_%H%M%S) # logfiles of download script will be saved here
-TIMESTAMP_BACKUP=$(/bin/date +%Y%m%d) # this will be used for naming the backup directory
-CONDAPATH="$HOME/opt/anaconda3" # conda environment for running the download script resides here
 SCRIPTPATH="$HOME/ICEBIRD/WEATHER/scripts" # path to this script
-LOGFILEPATH="$HOME/ICEBIRD/WEATHER/logfiles" # path for logfiles of download
-DATAPATH="$HOME/ICEBIRD/WEATHER/data" # path to which data will be downloaded
-BACKUPPATH="$HOME/ICEBIRD/WEATHER/daily_data/data_$TIMESTAMP_BACKUP" # path where backup will be saved
 
+source $SCRIPTPATH/config.bash
 ## RUN DOWNLOAD
 source $CONDAPATH/bin/activate $CONDAPATH/envs/icebird # activate the conda environment
 python -W ignore $SCRIPTPATH/download_ftp_icebird.py>>$LOGFILEPATH/logfile_$TIMESTAMP_LOGFILE.txt # run download script
